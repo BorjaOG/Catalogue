3435b5b532912aebb253a6ea6ba0641e01888e04
Borja Orge Gesteira <borja_1089@hotmail.com>
2023-03-28 18:57:41 +0800

    paremeter

--- Service/DataAcces.cs
+++ Service/DataAcces.cs
@@ -58,6 +58,7 @@ namespace Service
             }
         }
 
+        //Parameter combobox//
         public void setearParametro(string nombre, object valor)
         {
             comand.Parameters.AddWithValue(nombre, valor);
